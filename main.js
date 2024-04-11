838b4db786d879109fcf0afb30e2254474af8fe2
inseungjin <tmdwls1495@naver.com>
2024-04-12 04:56:59 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = []
+addButton.addEventListener("click", addTask)
+
+function addTask(){
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+         isComplete: false
+    }
+    let taskContent = taskInput.value
+    taskList.push(taskContent)
+    render();
+}
+
+function render(){
+    let resultHTML = ''
+    for(let i=0;i<taskList.length;i++){
+        if(taskList[i].isComplete == true) {
+            resultHTML += `<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>
+                <div>
+                    <button onclick = "toggleComplete('${taskList[i].id}')">Check</button>
+                    <button onclick = "deleteTask()">Delete</button>
+                </div>
+             </div>`;
+        } else {
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
+                <div>
+                    <button onclick = "toggleComplete('${taskList[i].id}')">Check</button>
+                    <button onclick = "deleteTask()">Delete</button>
+                </div>
+            </div>`;
+        }
+    }
+    document.getElementById("task-board").innerHTML = resultHTML;
+}
+function toggleComplete(id) {
+    for(let i = 0; i < taskList.length; i++) {
+        if(taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+     
+}
+
+function deleteTask(){
+    console.log("삭제")
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
